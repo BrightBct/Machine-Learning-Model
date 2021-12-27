eab8328f740874cbad63c8dd5c6421d36a80088d
Peemapat Podsoonthorn <bcbright46@gmail.com>
2021-12-27 15:27:34 +0700

    Test Model

---
+++ test_model.py
@@ -0,0 +1,7 @@
+import supervised_learning.classification.naive_bayes_model as naive
+import supervised_learning.regression.linear_regression_model as linear
+import unsupervised_learning.clustering.k_means_model as k_means
+
+Naive = naive.NaiveBayesModel()
+Linear = linear.LinearRegressionModel()
+K_Means = k_means.KMeansModel()
